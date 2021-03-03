3a68bd82e24380fa9ac92241bea0fcbeb341bfc5
robdcampbell <rdeancampbell@gmail.com>
2021-03-03 10:18:05 -0500

    add information for DataStructure: Stack

--- main.js
+++ main.js
@@ -4,11 +4,13 @@
 const toggleSortOptions = document.getElementById("sorting__toggle");
 const toggleArrayOptions = document.getElementById("array__toggle");
 const toggleGeneralOptions = document.getElementById("general__toggle");
+const toggleDataStructOptions = document.getElementById("dataStruct__toggle");
 
 // event listeners for toggling
 toggleSortOptions.addEventListener("click", sortOptionsMenu);
 toggleArrayOptions.addEventListener("click", sortOptionsMenu);
 toggleGeneralOptions.addEventListener("click", sortOptionsMenu);
+toggleDataStructOptions.addEventListener("click", sortOptionsMenu);
 
 function sortOptionsMenu(e) {
   e.target.nextElementSibling.classList.toggle("hidden");
