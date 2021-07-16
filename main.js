3cadcf69a503da1bbcbf24233718ef39c5f57e1e
robdcampbell <rdeancampbell@gmail.com>
2021-07-16 14:38:49 -0400

    Begin Big O section

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 //
 //
 // side nav buttons to show/hide
+const toggleBigO = document.getElementById("bigo__toggle");
 const toggleSortOptions = document.getElementById("sorting__toggle");
 const toggleArrayOptions = document.getElementById("array__toggle");
 const toggleGeneralOptions = document.getElementById("general__toggle");
 
 // event listeners for toggling
+toggleBigO.addEventListener("click", sortOptionsMenu);
 toggleSortOptions.addEventListener("click", sortOptionsMenu);
 toggleArrayOptions.addEventListener("click", sortOptionsMenu);
 toggleGeneralOptions.addEventListener("click", sortOptionsMenu);
