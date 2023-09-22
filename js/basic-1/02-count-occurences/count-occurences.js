044d950c8671d081401e7235801e0ed9b6f543c1
robdcampbell <rdeancampbell@gmail.com>
2023-09-22 16:23:07 -0400

    Add Nodemon for terminal testing

--- js/basic-1/02-count-occurences/count-occurences.js
+++ js/basic-1/02-count-occurences/count-occurences.js
@@ -1,1 +1,15 @@
-console.log("baked lays");
+// console.log("baked lays");
+
+function countOccurences(str, char) {
+  let count = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    if (char === str[i]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countOccurences("hello", "o")); // 2;
+// countOccurences('hello', 'z') // 0;
